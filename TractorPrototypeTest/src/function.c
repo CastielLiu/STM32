8d616772025293c2ebe5ca6b43834eb9fa2b9c08
castiel_liu <castiel_liu@outlook.com>
2019-01-18 13:55:10 +0800

    ..

--- TractorPrototypeTest/src/function.c
+++ TractorPrototypeTest/src/function.c
@@ -17,10 +17,6 @@ void clearBKP(void)
 }
 
 
-#define DRIVERLESS_STATE 1
-#define MANUAL_DRIVE_STATE 0
-#define PAUSE_DRIVERLESS_STATE 2
-
 void startDriverless()
 {
 	u8 buf[2]={4,DRIVERLESS_STATE};
@@ -47,7 +43,7 @@ void recordTargetPoint()
 	char show_lon[12];
 	char show_lat[12];
 	float	dis ;
-	u8 can3C0Buf = RECORD_TARGET_POINT;
+	u8 answerBuf[2] = {RECORD_TARGET_POINT};
 	
 	if(g_recordTargetSeq ==0 )
 		dis= g_MinDisBetweenTwoTarget  +1.5;//距离设置为大于record_dis_threshold的值
@@ -74,10 +70,11 @@ void recordTargetPoint()
 			LCD_ShowString(LCD_LU_X+6*LCD_FOND_SIZE/2,LCD_LU_Y + LCD_FOND_SIZE*2+(g_recordTargetSeq)*LCD_FOND_SIZE*2,11*LCD_FOND_SIZE/2,LCD_FOND_SIZE,LCD_FOND_SIZE,show_lat);
 		
 			g_recordTargetSeq++;	
+			answerBuf[1] = g_recordTargetSeq;
 			//printf("%f\t%f\tdis=%f\r\n",g_gps_sphere_now.lon*180/3.1415926,g_gps_sphere_now.lat*180/3.1415926,dis);
 		
-			//BEEP = 1; delay_ms(300); BEEP = 0; delay_ms(300); BEEP = 1 ; delay_ms(300); BEEP = 0; 
-			Can_Send_Msg(0x3C0,&can3C0Buf,1);
+			BEEP = 1; delay_ms(300); BEEP = 0; delay_ms(300); BEEP = 1 ; delay_ms(300); BEEP = 0; 
+			Can_Send_Msg(0x3C0,answerBuf,2);
 		}
 		
 	}
@@ -85,14 +82,18 @@ void recordTargetPoint()
 
 void steeringVerify()
 {
+	u8 answerBuf[1] = {STEER_VERIFY};
 	uint16_t angleSensorAdvalue = 0;
+	
 	while(0 == (angleSensorAdvalue = getAdcValue())) ;
 	*(__IO uint32_t *)((uint32_t)BKP_BASE + BKP_DR2) |= 0x000A; 
 			//BKP_DR2  最低字节为A 表示已经进行了转角传感器数据校准，初始化时检查此寄存器
 	BKP_WriteBackupRegister(BKP_DR3,angleSensorAdvalue); //AD偏移值写入BKP_DR3
+	Can_Send_Msg(0x3C0,answerBuf,1);
 }
 void angleSensorVerify( const CanRxMsg *  RxMessage)
 {
+	u8 answerBuf[1] = {ANGLE_SENSOR_VERIFY};
 	uint16_t maxAdValue = RxMessage->Data[1]*256 + RxMessage->Data[2];
 	uint16_t maxAngle = RxMessage->Data[3]*256 + RxMessage->Data[4];
 	
@@ -103,6 +104,7 @@ void angleSensorVerify( const CanRxMsg *  RxMessage)
 	//BKP_DR2 次低字节为A 表示已经进行了转角传感器参数设定
 	BKP_WriteBackupRegister(BKP_DR4,maxAdValue); //最大AD值
 	BKP_WriteBackupRegister(BKP_DR5,maxAngle); // 最大角度值 其中最高位为 方向
+	Can_Send_Msg(0x3C0,answerBuf,1);
 }
 
 void steeringDebug(const CanRxMsg * RxMessage)
--- TractorPrototypeTest/src/steering_motor.c
+++ TractorPrototypeTest/src/steering_motor.c
@@ -214,7 +214,7 @@ static const float degreePerCycle = 20.0;
 void steer_control(float angle_diff)
 {
 	float cycleNum = - angle_diff / degreePerCycle;
-	setSteeringSpeed(10); //10rpm
+	setSteeringSpeed(20); //10rpm
 	delay_ms(1);
 	setSteeringRotate(cycleNum);
 }
